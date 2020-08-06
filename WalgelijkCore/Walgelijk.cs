04c53192f443d5bef3778c9af71e4aa8d5817239
Mestiez <m.pereira@student.fontys.nl>
2020-08-06 13:05:58 +0200

    eerste commit

---
+++ WalgelijkCore/Walgelijk.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace WalgelijkCore
+{
+    public class Walgelijk
+    {
+        public Window Window { get; }
+
+        public Walgelijk(Window window)
+        {
+            Window = window;
+        }
+
+        public void Start()
+        {
+
+        }
+    }
+
+    public abstract class Window
+    {
+        public abstract Vector2 Position { get; set; }
+        public abstract Vector<int> Size { get; set; }
+        public abstract bool IsOpen { get; }
+        public abstract bool HasFocus { get; }
+
+        public Window()
+        {
+
+        }
+    }
+}
