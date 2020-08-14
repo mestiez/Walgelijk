6dfc98ae6856024b89c5e7e3d0ba9fb61f5bea9f
Mestiez <m.pereira@student.fontys.nl>
2020-08-15 00:04:39 +0200

    ECS start hel

---
+++ Walgelijk/Entities/Scene.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Walgelijk.Entities
+{
+    /// <summary>
+    /// Stores and manages components and systems
+    /// </summary>
+    public sealed class Scene
+    {
+        // TODO alles hier. maak een manier om components uit entities te halen en alles en alles en alles
+    }
+
+    public interface ISystem
+    {
+        void Execute();
+        void Render();
+    }
+
+    public struct Entity
+    {
+        public int Identity { get; set; }
+    }
+
+    public struct TransformComponent
+    {
+        public Vector2 Position { get; set; }
+        public float Rotation { get; set; }
+    }
+}
