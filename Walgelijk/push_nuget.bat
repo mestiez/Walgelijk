a6761ee2c3dcd6202cd71f42d1ff248bc2a6c2fb
Mestiez <mestiez@outlook.com>
2022-05-30 00:24:24 +0200

    versioning

--- Walgelijk/push_nuget.bat
+++
@@ -1,7 +0,0 @@
-echo off
-set version=%1
-set p=bin/Release/Walgelijk.%1%.nupkg
-
-echo Uploading %version% to %p%
-
-dotnet nuget push -s http://78.141.216.223:5000/v3/index.json -k "y63K&Zh4e" %p%
