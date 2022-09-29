d6e2189404878425558e62aef0fdeb78067f6ef6
zooi <32131519+mestiez@users.noreply.github.com>
2022-09-29 13:05:29 +0200

    Window fullscreen business and text rendering improvements

---
+++ Walgelijk/Window/WindowType.cs
@@ -0,0 +1,24 @@
+﻿namespace Walgelijk;
+
+/// <summary>
+/// The window border type
+/// </summary>
+public enum WindowType
+{
+    /// <summary>
+    /// Default window
+    /// </summary>
+    Normal,
+    /// <summary>
+    /// Borderless window (why)
+    /// </summary>
+    Borderless,
+    /// <summary>
+    /// Borderless fullscreen
+    /// </summary>
+    BorderlessFullscreen,
+    /// <summary>
+    /// Fullscreen
+    /// </summary>
+    Fullscreen,
+}
