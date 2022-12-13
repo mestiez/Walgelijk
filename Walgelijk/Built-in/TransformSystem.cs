ae4ea6a712d876008fcd8c09fd6a2e1052ebc6e2
zooi <32131519+mestiez@users.noreply.github.com>
2022-12-13 13:29:44 +0100

    transform system fix

--- Walgelijk/Built-in/TransformSystem.cs
+++ Walgelijk/Built-in/TransformSystem.cs
@@ -21,16 +21,24 @@ namespace Walgelijk
         /// </summary>
         public bool Parenting = true;
 
+        public readonly int Capacity = 4096;
+
+        public TransformSystem(int capacity = 4096)
+        {
+            Capacity = capacity;
+        }
+
         public override void Update()
         {
-            var arr = ArrayPool<TransformComponent>.Shared.Rent(4096);
+            var arr = ArrayPool<TransformComponent>.Shared.Rent(Capacity);
             var all = Scene.GetAllComponentsOfType(arr);
 
-            foreach (var transform in all)
+            for (int i = 0; i < all.Length; i++)
             {
-                if (Parenting)
+                var transform = all[i];
+                if (Parenting && !transform.Parent.HasValue)
                     CascadeMatrixCalculation(transform, all);
-                else if (!transform.Parent.HasValue)
+                else
                     CalculateMatrix(transform);
             }
 
@@ -45,21 +53,25 @@ namespace Walgelijk
                 transform.RecalculateModelMatrix(Matrix3x2.Identity);
         }
 
-        private void CascadeMatrixCalculation(TransformComponent transform, ReadOnlySpan<TransformComponent> collection, TransformComponent? up = null)
+        private void CascadeMatrixCalculation(TransformComponent transform, ReadOnlySpan<TransformComponent> all, TransformComponent? up = null)
         {
             bool shouldRecalculate = !transform.IsMatrixCached;
 
             if (shouldRecalculate)
                 transform.RecalculateModelMatrix(up?.LocalToWorldMatrix ?? Matrix3x2.Identity);
 
-            foreach (var e in collection)
+            // waarom heeft een transform geen lijst met kinderen? dat is sneller toch? 
+            // helaas is het niet zo simpel. de lijst is dat een List<Entity> (waarschijnlijk) en die moet bijgehouden worden als een entity niet meer bestaat of als die entity geen Transform meer heeft.
+            // dat is extra overhead. of dit het waard is is een ander verhaal. als je dat wilt meten, ga je gang, ik heb daar nu geen zin in.
+            for (int i = 0; i < all.Length; i++)
             {
+                var e = all[i];
                 var myParent = e.Parent;
 
                 if (myParent.HasValue && myParent.Value != e.Entity && myParent.Value == transform.Entity)
                 {
                     e.IsMatrixCached &= !shouldRecalculate;
-                    CascadeMatrixCalculation(e, collection, transform);
+                    CascadeMatrixCalculation(e, all, transform);
                 }
             }
         }
