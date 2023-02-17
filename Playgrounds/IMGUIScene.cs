81a9e8464ccad59550e3e62009e290e83e33783e
mestiez <32131519+mestiez@users.noreply.github.com>
2023-02-17 04:05:51 +0100

    onion control tree progress

--- Playgrounds/IMGUIScene.cs
+++ Playgrounds/IMGUIScene.cs
@@ -1,6 +1,7 @@
 ﻿using System.Numerics;
 using Walgelijk;
 using Walgelijk.Imgui;
+using Walgelijk.Onion;
 
 namespace TestWorld;
 
@@ -11,6 +12,7 @@ public struct IMGUIScene : ISceneCreator
         var scene = new Scene(game);
         scene.AddSystem(new TransformSystem());
         scene.AddSystem(new GuiSystem());
+        scene.AddSystem(new OnionSystem());
         scene.AddSystem(new IMGUITestSystem());
         scene.AddSystem(new CameraSystem() { ExecutionOrder = -1 });
         var camera = scene.CreateEntity();
@@ -34,8 +36,34 @@ public struct IMGUIScene : ISceneCreator
 
         public override void Update()
         {
-            if (Gui.ClickButton("Hello World", new Vector2(32), new Vector2(128,32), HorizontalTextAlign.Center, VerticalTextAlign.Middle))
-                Audio.PlayOnce(Sound.Beep);
+            //if (Gui.ClickButton("Hello World", new Vector2(32), new Vector2(128, 32), HorizontalTextAlign.Center, VerticalTextAlign.Middle))
+            //    Audio.PlayOnce(Sound.Beep);
+
+            Onion.Tree.Start(23, null);
+            Onion.Tree.Start(5, null);
+            Onion.Tree.End();
+            Onion.Tree.Start(635, null);
+            Onion.Tree.End();
+            Onion.Tree.Start(6235, null);
+            Onion.Tree.End();
+            Onion.Tree.End();
+
+            if (Input.IsKeyHeld(Key.K))
+            {
+                Onion.Tree.Start(425, null);
+                Walgelijk.Onion.Button.Click("Hallo wereld!", default, new Vector2(128, 32));
+                Onion.Tree.End();
+            }
+
+                Walgelijk.Onion.Button.Click("Hallo wereld!", new Vector2(512,256), new Vector2(128, 32));
+            Onion.Tree.Start(75, null);
+            Onion.Tree.End();
+
+            Onion.Tree.Start(535, null);
+            Onion.Tree.End();
+
+            Onion.Tree.Start(42, null);
+            Onion.Tree.End();
         }
 
         public override void Render()
