74bc13121bc0b998233845d2f1b4e098161b02b9
zooi <32131519+mestiez@users.noreply.github.com>
2023-03-23 13:32:00 +0100

    added group control

--- Walgelijk.Onion/Controls/Dummy.cs
+++ Walgelijk.Onion/Controls/Dummy.cs
@@ -1,17 +1,9 @@
 ﻿using Walgelijk.Onion.Layout;
-using Walgelijk.SimpleDrawing;
 
 namespace Walgelijk.Onion.Controls;
 
 public readonly struct Dummy : IControl
 {
-    public readonly bool DrawBackground;
-
-    public Dummy(bool drawBackground = false)
-    {
-        DrawBackground = drawBackground;
-    }
-
     public void OnAdd(in ControlParams p)
     {
     }
@@ -28,36 +20,10 @@ public readonly struct Dummy : IControl
     public void OnProcess(in ControlParams p)
     {
         p.Instance.Rects.Rendered = p.Instance.Rects.ComputedGlobal;
-        if (DrawBackground)
-        {
-            p.Instance.CaptureFlags = CaptureFlags.Hover;
-            p.Instance.Rects.Raycast = p.Instance.Rects.Rendered;
-            p.Instance.Rects.DrawBounds = p.Instance.Rects.Rendered;
-        }
     }
 
     public void OnRender(in ControlParams p)
     {
-        if (DrawBackground)
-        {
-            (ControlTree tree, Layout.Layout layout, Input input, GameState state, Node node, ControlInstance instance) = p;
-
-            instance.Rects.Rendered = instance.Rects.ComputedGlobal;
-            var t = node.GetAnimationTime();
-
-            if (t <= float.Epsilon)
-                return;
-
-            var anim = instance.Animations;
-
-            var fg = Onion.Theme.Foreground;
-            Draw.Colour = fg.Color;
-            Draw.Texture = fg.Texture;
-
-            anim.AnimateRect(ref instance.Rects.Rendered, t);
-            anim.AnimateColour(ref Draw.Colour, t);
-            Draw.Quad(instance.Rects.Rendered, 0, Onion.Theme.Rounding);
-        }
     }
 
     public void OnEnd(in ControlParams p) { }
