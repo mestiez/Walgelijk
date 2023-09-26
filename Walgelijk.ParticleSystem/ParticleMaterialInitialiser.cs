456db4e8b34d65613c869913ced63280be9191f9
zooi <32131519+mestiez@users.noreply.github.com>
2023-09-26 13:06:09 +0200

    apparently i cant count

--- Walgelijk.ParticleSystem/ParticleMaterialInitialiser.cs
+++ Walgelijk.ParticleSystem/ParticleMaterialInitialiser.cs
@@ -10,17 +10,17 @@ public static class ParticleMaterialInitialiser
 
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec2 texcoord;
-layout(location = 3) in vec2 normal;
-layout(location = 4) in vec4 color;
+layout(location = 2) in vec3 normal;
+layout(location = 3) in vec4 color;
 
-layout(location = 5) in mat4 particleModel;
+layout(location = 4) in mat4 particleModel;
 layout(location = 8) in vec4 particleColor;
 layout(location = 9) in vec4 particleUvOffset;
 
 out vec2 uv;
 out vec4 vertexColor;
 
-uniform mat4 model;
+uniform mat4 model;d
 uniform mat4 view;
 uniform mat4 projection;
 
