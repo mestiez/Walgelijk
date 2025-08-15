c496a5c827909150db31980b10da13888211fc33
zooi <32131519+mestiez@users.noreply.github.com>
2025-08-15 19:15:50 +0200

    Update TextureDeserialiser.cs

--- Walgelijk.AssetManager/Deserialisers/TextureDeserialiser.cs
+++ Walgelijk.AssetManager/Deserialisers/TextureDeserialiser.cs
@@ -6,7 +6,7 @@ public class TextureDeserialiser : IAssetDeserialiser<Texture>
     {
         using var b = stream();
         var buffer = new byte[assetMetadata.Size];
-        b.Read(buffer);
+        b.ReadExactly(buffer);
         return TextureLoader.FromBytes(buffer);
     }
 
